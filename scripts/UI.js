4955a7a4f4833ff03df6795f5cc611b79829bb50
just-me- <hess-marcel@bluewin.ch>
2018-04-07 20:42:45 +0200

    js resizer

--- scripts/UI.js
+++ scripts/UI.js
@@ -1,1 +1,8 @@
 /* hier sind DOM Verbindungen erlaubt */
+
+window.addEventListener("load", function() {
+    document.querySelector("#font-slider").addEventListener("change", function() {
+        console.log("did"+this.value);
+        document.querySelector("html").style.fontSize = this.value/10+"em";
+    });
+});
