1aa58fd8ee471c2ad8379db85863de69b321b248
just-me- <hess-marcel@bluewin.ch>
2018-04-07 20:51:07 +0200

    Js fix UI feedback

--- scripts/UI.js
+++ scripts/UI.js
@@ -1,8 +1,7 @@
 /* hier sind DOM Verbindungen erlaubt */
-
 window.addEventListener("load", function() {
     document.querySelector("#font-slider").addEventListener("change", function() {
-        console.log("did"+this.value);
         document.querySelector("html").style.fontSize = this.value/10+"em";
+        document.querySelector("#font-size").innerText = this.value/10;
     });
 });
